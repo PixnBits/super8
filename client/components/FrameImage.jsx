3695115a3d5dcdb2f1e023b37afb365d74cbebe6
N Oliver <github@pixnbits.org>
2019-10-11 22:12:45 -0700

    fix(client): avoid reflow when reloading the preview

--- client/components/FrameImage.jsx
+++ client/components/FrameImage.jsx
@@ -3,25 +3,25 @@ import React, { useState, useEffect } from 'react';
 import comms from '../comms';
 
 function FrameImage() {
-  // view --> reset --> view
-  const [showFrame, setShowFrame] = useState(true);
+  const [imgSrc, setImgSrc] = useState(0);
   useEffect(() => {
     const listener = () => {
-      console.log('frame notification');
-      // showFrame := true we need to cycle false then later back to true
-      // showFrame := false already in process so noop
-      if (showFrame === false) {
-        return;
-      }
-      setShowFrame(false);
-      setTimeout(() => setShowFrame(true), 200);
+      // NOTE: this doesn't account for multiple instances of the component
+      // (if >1 component they may use different URLs, loading the same data
+      // multiple times)
+      const newSrc = `/frame.jpg?t=${Date.now()}`;
+      // first load the image, then show it
+      const img = new Image();
+      img.onload = () => setImgSrc(newSrc);
+      // if there's an error not sure it'd be helpful to change the image
+      img.src = newSrc;
     };
     comms.addEventListener('frame', listener);
     return () => comms.removeEventListener('frame', listener);
   });
   return (
     <p>
-      <img className="img-fluid" src={showFrame ? '/frame.jpg' : ''} />
+      <img className="img-fluid" src={imgSrc} />
     </p>
   );
 }
