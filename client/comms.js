8a4ff1ab771ec81885aeb0bbdc21000c454888aa
N Oliver <github@pixnbits.org>
2019-10-16 16:38:57 -0700

    feat(client): attempt to reconnect

--- client/comms.js
+++ client/comms.js
@@ -1,26 +1,54 @@
-// host includes the port, hostname does not
-const ws = new WebSocket(`ws://${window.location.host}`);
-
 const notifications = new EventTarget();
 
-// TODO: handle state updates
-ws.addEventListener('message', (event) => {
-  console.log('message:', event.data);
+function dispatchNotification(name, otherData = {}) {
+  const notificationEvent = new Event(name);
+  notificationEvent.notification = otherData;
+  notifications.dispatchEvent(notificationEvent);
+}
 
-  let message;
-  try {
-    message = JSON.parse(event.data);
-  } catch (err) {
-    console.warn('unable to parse incoming server message', event.data, err);
-    return;
-  }
+let ws;
+const MINIMUM_RETRY_DELAY = 2e3;
+const MAXIMUM_RETRY_DELAY = 5 * 60 * 1e3;
+function setupWebSocket(timeToWaitBetweenAttempts = MINIMUM_RETRY_DELAY) {
+  // host includes the port, hostname does not
+  ws = new WebSocket(`ws://${window.location.host}`);
 
-  if ('notification' in message) {
-    const notificationEvent = new Event(message.notification);
-    notificationEvent.notification = message;
-    notifications.dispatchEvent(notificationEvent);
-  }
-});
+  // TODO: handle state updates
+  ws.addEventListener('message', (event) => {
+    console.log('message:', event.data);
+
+    let message;
+    try {
+      message = JSON.parse(event.data);
+    } catch (err) {
+      console.warn('unable to parse incoming server message', event.data, err);
+      return;
+    }
+
+    if ('notification' in message) {
+      dispatchNotification(message.notification, message);
+    }
+  });
+
+  let nextTimeToWaitBetweenAttempts = Math.floor(
+    Math.min(
+      MINIMUM_RETRY_DELAY,
+      Math.max(
+        MINIMUM_RETRY_DELAY,
+        1.2 * (0.9 + 0.2 * Math.random()) * timeToWaitBetweenAttempts
+      )
+    )
+  );
+  ws.addEventListener('open', () => {
+    nextTimeToWaitBetweenAttempts = MINIMUM_RETRY_DELAY;
+    dispatchNotification('commsOpen');
+  });
+  ws.addEventListener('close', () => {
+    setTimeout(() => setupWebSocket(nextTimeToWaitBetweenAttempts), timeToWaitBetweenAttempts);
+    dispatchNotification('commsClosed');
+  });
+}
+setupWebSocket();
 
 // args can be an object for key/value pairs
 // args can be an array for positional values
