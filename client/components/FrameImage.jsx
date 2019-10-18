bf15d3cee9c9c10a657e916caeb3143fce1eaa06
N Oliver <github@pixnbits.org>
2019-10-17 20:48:25 -0700

    fix(client): FrameImage initial image src

--- client/components/FrameImage.jsx
+++ client/components/FrameImage.jsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 import comms from '../comms';
 
+function getNewFrameSrc() {
+  return `/frame.jpg?t=${Date.now()}`;
+}
+
 function FrameImage() {
-  const [imgSrc, setImgSrc] = useState(0);
+  const [imgSrc, setImgSrc] = useState(getNewFrameSrc);
   useEffect(() => {
     const listener = () => {
       // NOTE: this doesn't account for multiple instances of the component
       // (if >1 component they may use different URLs, loading the same data
       // multiple times)
-      const newSrc = `/frame.jpg?t=${Date.now()}`;
-      setImgSrc(newSrc);
+      setImgSrc(getNewFrameSrc());
     };
     comms.addEventListener('frame', listener);
     return () => comms.removeEventListener('frame', listener);
